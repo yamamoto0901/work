c75f81599ec6a56e6647386e497b78171bf08b52
yamamoto0901 <71079144+yamamoto0901@users.noreply.github.com>
2020-11-03 16:19:11 +0900

    Add files via upload

---
+++ site/main.js
@@ -0,0 +1,24 @@
+$(function(){
+    $(window).scroll(function (){
+      $('.fadein').each(function(){
+        var elementTop = $(this).offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll > elementTop - windowHeight + 100){
+          $(this).addClass('scrollin');
+        }
+      });
+    });
+
+ 
+    $('.slick').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear'
+    });
+
+
+  });
+

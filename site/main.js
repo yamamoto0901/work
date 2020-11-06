fa876e5e89846f00ab72798c0494c3a514d8bd5d
yamamoto0901 <kamui9610.yy@gmail.com>
2020-11-06 19:57:13 +0900

    追加

--- site/main.js
+++ site/main.js
@@ -1,24 +1,12 @@
-$(function(){
-    $(window).scroll(function (){
-      $('.fadein').each(function(){
-        var elementTop = $(this).offset().top;
-        var scroll = $(window).scrollTop();
-        var windowHeight = $(window).height();
-        if (scroll > elementTop - windowHeight + 100){
-          $(this).addClass('scrollin');
-        }
-      });
+$(function() {
+  $(window).scroll(function() {
+    $(".fadein,.fadein2,.fadein3").each(function() {
+      var elementTop = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > elementTop - windowHeight + 100) {
+        $(this).addClass("scrollin");
+      }
     });
-
- 
-    // $('.slick').slick({
-    //   dots: true,
-    //   infinite: true,
-    //   speed: 500,
-    //   fade: true,
-    //   cssEase: 'linear'
-    // });
-
-
   });
-
+});
